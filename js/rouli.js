d23f2ac3010b65ad5cd1015b696b87bfbdd565e7
Rouli <roulili.la.terreur@gmail.com>
2015-10-23 16:07:57 +0200

    reprise des commits blocus, landing et maths sont OK

--- js/rouli.js
+++ js/rouli.js
@@ -1,4 +1,17 @@
 $('.section-photo-etudes').height($('#photo-etudes').height());
-
 var m = 160/1920*$('#section-etudes-background').width();
 $('.section-etudes').css({'margin-top': -m});
+
+$('.section-photo-cours').height($('#photo-cours').height());
+var m = 180/1920*$('#section-cours-background').width();
+$('.section-cours').css({'margin-top': -m});
+
+$('.section-photo-blocus').height($('#photo-blocus').height());
+var m = 180/1920*$('#section-blocus-background').width();
+$('.section-blocus').css({'margin-top': -m});
+
+$('.section-photo-stages').height($('#photo-stages').height());
+var m = 180/1920*$('#section-stages-background').width();
+$('.section-stages').css({'margin-top': -m});
+
+$('.section-carte').height($('#section-carte-background').height());
