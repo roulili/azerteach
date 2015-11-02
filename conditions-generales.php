094b533352df118b3f66538245655dc808333f70
Rouli <roulili.la.terreur@gmail.com>
2015-11-02 11:53:14 +0100

    ajout conditions générales

---
+++ conditions-generales.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <!-- Google fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800' rel='stylesheet' type='text/css'>
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="css/custom.css">
+  </head>
+  <body id="contact">
+
+<?php include('includes/navbar.php'); ?>
+
+
+<div class="section section-contact-top">
+  <div class="container">
+    <img src="img/logo.png" id="matiere-logo" alt="logo"/>
+    <h2 class="page-title">Condictions générales de service</h2>
+  </div>
+</div>
+    <div class="section section-contact-form">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-md-offset-2 matiere-details cgu">
+            <h3>1. Acceptation des conditions générales</h3>
+            <p>
+              Les présentes conditions générales de vente règlent l’ensemble des relations entre Azerteach asbl et ses clients.<br />
+              Au moment de son inscription à une activité Azerteach, le client adhère intégralement et sans réserve aux présentes conditions générales.<br />
+              Nous nous réservons le droit de modifier à tout moment les présentes conditions générales de vente. Dans cette hypothèse, nous mettrons directement en ligne, sur notre site www.azerteach.com, les nouvelles conditions générales. Toute inscription effectuée après la publication des conditions mises à jour vaudra acceptation pleine et entière de celles-ci
+            </p>   
+            <h3>2. Prestations de services</h3>
+            <p>
+              Azerteach asbl organise différentes activités de soutien scolaire pour les élèves du primaire, du secondaire ainsi que pour les étudiants du supérieur. En vue d’assurer la prestation de ses activités, l’association collabore régulièrement avec des professeurs extérieurs. L’ensemble des activités ont lieu au 67, avenue Winston Churchill à Uccle.
+            </p>
+            <h3>3. Organisations des activités</h3>
+            <p>
+              Dans le cadre des présentes conditions générales, il convient de préciser la nature des activités Azerteach et les périodes durant lesquelles ces dernières sont organisées :
+            </p>
+            <ul class="blue-dot-grey-text">
+              <li><span class="text-grey">Cours particuliers : durant l’année scolaire (y compris pendant les vacances)</span></li>
+              <li><span class="text-grey">Études encadrées : chaque mercredi et samedi, de 14h à 17h (hors vacances scolaires)</span></li>
+              <li><span class="text-grey">Stages de vacances : durant les vacances scolaires</span></li>
+              <li><span class="text-grey">Blocus encadrés : lors de chaque période de blocus et durant les vacances de Pâques</span></li>
+            </ul>
+            <h3>4. Inscriptions</h3>
+            <p>
+              L’inscription à une ou plusieurs activités Azerteach s’effectue par e-mail à l’adresse hello@azerteach.com ou par téléphone au 02/345.48.37. Suite à votre demande, nous vous demanderons de payer l’intégralité du prix de l’activité, par virement bancaire ou par espèces, au plus tard le premier jour du début de l’activité. En cas de versement bancaire, nous vous demanderons d’en apporter la preuve.
+            </p>
+            <p>
+              L’inscription à l’activité ne sera effective que dès réception du paiement. En cas de non-paiement, nous nous réservons le droit de suspendre votre participation à l’activité.
+            </p>
+            <p>
+              Une activité est ouverte quel que soit le nombre des participants, à l’exception des stages de vacances où le nombre minimum de 3 inscrits est requis. Dans ce dernier cas, en cas d’inscriptions insuffisantes, nous annulerons l’activité et nous vous rembourserons intégralement.
+            </p>
+            <p>
+              Nous nous réservons le droit d’annuler une activité. Dans ce cas, nous vous proposerons de déplacer l’activité ou de vous rembourser intégralement.
+            </p> 
+
+            <h3>5. Absences et annulations</h3>
+            <p>
+              Toute annulation d’une activité s’effectue par e-mail à l’adresse hello@azerteach.com ou par téléphone au 02/345.48.37.
+            </p>
+            <h4>5.1. Cours particuliers</h4>
+            <p>
+              Il vous est possible d’annuler un cours au moins 48h avant le début du cours. Lorsque le délai d’annulation est respecté, nous vous proposerons de déplacer votre cours ou de vous rembourser intégralement s’il y a lieu.
+            </p>
+            <p>
+              En cas de non respect des délais d’annulation, aucun déplacement ni remboursement du cours ne pourront être faits. Par ailleurs, en cas de non paiement anticipé, nous nous réservons le droit d’exiger le versement d’une indemnité compensatoire équivalant au prix du cours.
+            </p>
+            <h4>5.2. Étude encadrées</h4>
+            <p>
+              Il vous est possible d’annuler une séance d’étude encadrée au moins 48h avant le début de l’étude encadrée. Lorsque le délai d’annulation est respecté, nous vous proposerons de rattraper votre séance la semaine suivante ou de vous rembourser intégralement s’il y a lieu.
+            </p>
+            <p>
+              En cas de non respect des délais d’annulation, aucun rattrapage ni remboursement de la séance ne pourront êtres faits. Par ailleurs, en cas de non paiement anticipé, nous nous réservons le droit d’exiger le versement d’une indemnité compensatoire équivalent au prix de la séance.
+            </p>
+            <h4>5.3. Stages de vacances</h4>
+            <p>
+              Il vous est possible d’annuler un stage de vacances au moins 4 semaines avant le premier jour du stage. Lorsque le délai d’annulation est respecté, nous vous rembourserons intégralement le prix du stage s’il y a lieu.
+            </p>
+            <p>
+              En cas de non respect des délais, nous nous réservons le droit d’exiger le versement d’une indemnité compensatoire. Le montant de cette indemnité sera fonction de la date d’annulation :
+            </p>
+            <ul>
+              <li><span class="text-grey">Plus de 2 semaines à l’avance : 50 % du prix du stage ;</span></li>
+              <li><span class="text-grey">Plus d’1 semaine à l’avance : 75 % du prix du stage ;</span></li>
+              <li><span class="text-grey">Moins d’une semaine à l’avance : 100 % du prix du stage.</span></li>
+            </ul>
+            <p>En cas de paiement anticipé, et selon le montant exigé, nous vous rembourserons la différence dans les plus brefs délais.</p>
+            <p>En cas d’absence durant une partie du stage, les jours manqués ne seront pas remboursés.</p>
+            <h4>5.4. Blocus encadré</h4>
+            <p>
+              Il vous est possible d’annuler un blocus encadré au moins 4 semaines avant le premier jour du blocus. Lorsque le délai d’annulation est respecté, nous vous rembourserons intégralement le prix du blocus encadré s’il y a lieu.
+            </p>
+            <p>
+              En cas de non respect des délais, nous nous réservons le droit d’exiger le versement d’une indemnité compensatoire. Le montant de cette indemnité sera fonction de la date d’annulation :
+            </p>
+            <ul>
+              <li><span class="text-grey">Plus de 2 semaines à l’avance : 50 % du prix du blocus ;</span></li>
+              <li><span class="text-grey">Plus d’1 semaine à l’avance : 75 % du prix du blocus ;</span></li>
+              <li><span class="text-grey">Moins d’une semaine à l’avance : 100 % du prix du blocus.</span></li>
+            </ul>
+
+            <p>
+              En cas de paiement anticipé, et selon le montant exigé, nous vous rembourserons la différence dans les plus brefs délais.
+            </p>
+            <p>En cas d’absence durant une partie du blocus encadré, les jours manqués ne seront pas remboursés.</p>
+            <h3>6. Prix</h3>
+            <p>
+              Les prix mentionnés sur nos flyers, affiches et site web sont purement indicatifs. Ils incluent les taxes et autres frais divers encourus par l’association.
+            </p>
+            <p>
+              Nous nous réservons le droit de modifier les prix sans préavis. Toutefois, les prix des activités déjà réservées ou réalisées ne seront pas affectées par ces modifications, valables dans le futur.
+            </p>
+            <h3>7. Responsabilité</h3>
+
+            <p>
+              Azerteach asbl n’est tenue qu’à une obligation de moyen dans le cadre des services fournis aux élèves et étudiants. L’association ne pourra être tenue pour responsable du préjudice causé à l’élève ou l’étudiant par un de ses professeurs et, de manière plus générale, pour tout dommage subi ou causé à l’occasion d’une activité ou de la non prestation d’une activité.
+            </p>
+            <p>
+              Les locaux et le matériel mis à disposition des participants dans le cadre de leurs cours doivent être utilisés « en bon père de famille ». Toute dégradation sera portée en compte de la personne négligente.
+              Azerteach se réserve le droit d’interdire l’accès à un cours à un participant dont le comportement empêche les autres membres de son groupe, ou d’autres clients, de suivre leur cours de manière normale. Toute exclusion pour raison disciplinaire se fera sans remboursement ni indemnité.
+              Le client répond du préjudice causé à Azerteach, à ses préposés et / ou à ses représentants, par sa faute ou à la suite de la non-exécution de ses obligations contractuelles. La faute est appréciée par référence au comportement normal d’un participant à une formation.
+            </p>
+
+            <h3>8. Vie privée</h3>
+
+            <p>
+              A chaque élève inscrit à une activité, nous lui fournissons un formulaire de contact à compléter. Les données personnelles reprises dans ce formulaire sont incluses dans un fichier-client tenu dans les locaux de l’association situés au 67, avenue Winston Churchill à 1180 Uccle.
+            </p>
+            <p>
+              Nous garantissons la protection de ces données personnelles. Et nous nous engageons à ne pas les divulguer ni les vendre, louer ou échanger à toute autre organisation, société ou entité, sauf après avoir obtenu votre consentement exprès, ou si cela est requis par la loi.
+            </p>
+            <p>
+              A tout moment, vous pouvez obtenir la rectification de toute donnée erronée et/ou le retrait de ces données du fichier.
+            </p>
+            <h3>9. Dispositions générales</h3>
+
+            <p>
+              Les présentes conditions générales ont été rédigées conformément au droit belge. La nullité d’une clause n’entraîne pas la nullité de l’ensemble des conditions générales.
+            </p>
+            <p>
+              Tout différend auquel pourrait donner lieu l’interprétation ou l’exécution des présentes conditions sera, sauf prescription légale impérative en sens contraire, de la compétence exclusive des cours et tribunaux francophones du siège social de la société.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="section section-contact section-contact-contact">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-md-offset-2 text-center tel-contact">
+            <p>Plus d'informations et réservation des cours:</p>
+            <tel>02 345 48 37</tel>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="section section-map">
+      <img src="img/pointe-haute.png" class="background-image">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-md-offset-2 text-center map">
+            <img src="img/contact-map.png" class="contact-map" />
+          </div>
+        </div>
+      </div>
+    </div>
+
+<?php include('includes/footer.php'); ?>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script src="js/scrollspy.js"></script>
+    <script src="js/rouli.js"></script>
+  </body>
+</html>
