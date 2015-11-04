d46658c618203322a5436a0dff0029584f4b9221
Rouli <roulili.la.terreur@gmail.com>
2015-11-04 17:12:25 +0100

    philo responsive

--- philosophie.php
+++ philosophie.php
@@ -12,7 +12,7 @@
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800' rel='stylesheet' type='text/css'>
     <!-- font awesome -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-
+    <link rel="stylesheet" href="css/smartphone.css">
     <link rel="stylesheet" href="css/custom.css">
   </head>
   <body id="philo">
@@ -42,7 +42,8 @@
       </div>
     </div>
     <div class="section section-ribbon">
-      <img src="../img/ribbon.png" class="background-image">
+      <img src="../img/ribbon.png" class="background-image hidden-xs">
+      <img src="../img/ribbon-xs.png" class="background-image visible-xs-block">
       <div class="container">
         <div class="row">
           <div class="col-md-8 col-md-offset-2 text-center citation">
@@ -80,7 +81,8 @@
       </div>
     </div>
     <div class="section section-ribbon">
-      <img src="../img/ribbon.png" class="background-image">
+      <img src="../img/ribbon.png" class="background-image hidden-xs">
+      <img src="../img/ribbon-xs.png" class="background-image visible-xs-block">
       <div class="container">
         <div class="row">
           <div class="col-md-8 col-md-offset-2 text-center citation">
