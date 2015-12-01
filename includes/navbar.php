03b41300600d44c58a042abe728540c330a63b1f
Rouli <roulili.la.terreur@gmail.com>
2015-12-01 16:33:26 +0100

    retour code de suivi analytics

--- includes/navbar.php
+++ includes/navbar.php
@@ -11,6 +11,17 @@ setTimeout(ldinsp, 500); document.readyState != "complete" ? (window.attachEvent
 </script>
 <!-- End Inspectlet Embed Code -->
 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-55960615-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
 
 
 <nav class="navbar navbar-landing navbar-normal">
